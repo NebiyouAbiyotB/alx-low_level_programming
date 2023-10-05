1d4d9355110aedfbe8962ed6332079070c074114
NebiyouAbiyotB <nebiyouabiyot10@gmail.com>
2023-10-05 10:17:05 -0700

    C first

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,16 @@
-#include<studio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
+ * main - Entry point of the program
+ *
+ * Description: This program prints "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a newline using the `printf` function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+
