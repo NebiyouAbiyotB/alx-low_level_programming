e697aa65ea6395149f07fba0fa5a4883d021be4e
NebiyouAbiyotB <nebiyouabiyot10@gmail.com>
2023-10-05 10:35:33 -0700

    C first

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <studio.h>
 #include <unistd.h>
 
 /**
-* main - Entry point 
-*
-* Return: Always 1 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-	write(2, "and that piece of ait is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59); // 59 is the length of the message
+return (1);
 }
+
