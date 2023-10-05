404edfd67d3efac09b1c6f58ef9cf9f816386c09
NebiyouAbiyotB <nebiyouabiyot10@gmail.com>
2023-10-05 10:38:19 -0700

    C first

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 59); // 59 is the length of the message
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); // 59 is the length of the message
 return (1);
 }
 
