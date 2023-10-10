fc408570ab8ca242c9ccea302061f84ccff356ff
NebiyouAbiyotB <nebiyouabiyot10@gmail.com>
2023-10-10 12:00:03 -0700

    Another assignment

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,13 @@ int print_sign(int n)
 {
 	if (n > 0){
 	
-		_putchar('+');
+		printf('+');
 		return (1);
 	} else if (n == 0){
-		_putchar(0);
+		printf(0);
 		return (0);
 	} else if (n < 0){
-	
-		_putchar('-');
+		printf('-');
 		return (-1);
 	}
 }
